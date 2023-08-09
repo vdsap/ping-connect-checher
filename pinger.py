cdfd133763b77600697ad50b8fc052eac1dbfedb
vdsap <thevdsap@gmail.com>
2023-08-09 11:48:18 +0300

    added yaml

--- pinger.py
+++ pinger.py
@@ -4,14 +4,12 @@ import datetime
 
 import requests
 from loguru import logger
+from yaml import safe_load as load
 
-hosts = {'11400f': '100.100.220.108',
-         "11400f-win": '100.120.141.115',
-         'mi9t': '100.97.35.40',
-         'thuban': "100.82.21.66",
-         'broadwell': '100.111.126.67',
-         'sap-pc': "100.73.61.145"}
 
+async def conf_init():
+    with open('hosts.yaml', encoding='UTF-8') as f:
+        return load(f)
 
 async def check_ping(ip):
     logger.info(f'Pinging {ip}')
@@ -30,6 +28,9 @@ async def check_ping(ip):
 async def ping_compare(name, ip):
     ping1 = await check_ping(ip)
     timer = datetime.datetime.now()
+    requests.get(
+        f"https://api.telegram.org/bot1105929277:AAEHFVKbNdvPHNulpW-ywks8hozFpq3kNco/sendMessage?"
+        f"chat_id=-1001206104553&parse_mode=html&message_thread_id=619&text=Pinger for <b>{name}</b> started")
     while True:
         ping2 = await check_ping(ip)
         if ping2 != ping1:
@@ -40,7 +41,7 @@ async def ping_compare(name, ip):
                 try:
                     requests.get(
                         f"https://api.telegram.org/bot1105929277:AAEHFVKbNdvPHNulpW-ywks8hozFpq3kNco/sendMessage?"
-                        f"chat_id=-1001206104553&message_thread_id=619&text=🔴 {name}%20internet%20⬇️")
+                        f"chat_id=-1001206104553&parse_mode=html&message_thread_id=619&text=🔴 {name}%20internet%20⬇️")
                 except Exception as err:
                     logger.error(err)
                 ping1 = ping2
@@ -51,7 +52,7 @@ async def ping_compare(name, ip):
                 try:
                     requests.get(
                         f'https://api.telegram.org/bot1105929277:AAEHFVKbNdvPHNulpW-ywks8hozFpq3kNco/sendMessage?'
-                        f'chat_id=-1001206104553&message_thread_id=619&text=🟢 {name}%20internet%20⬆️%2C%20time%'
+                        f'chat_id=-1001206104553&parse_mode=html&message_thread_id=619&text=🟢 {name}%20internet%20⬆️%2C%20time%'
                         f'20offline%3A%20{t}')
                 except Exception as err:
                     logger.error(err)
@@ -61,17 +62,28 @@ async def ping_compare(name, ip):
 
 
 async def main():
-    try:
-        tasks = []
-        for host_name, host_ip in hosts.items():
-            tasks.append(asyncio.create_task(ping_compare(host_name, host_ip)))
-        while True:
-            await asyncio.sleep(1)
-    except KeyboardInterrupt:
-        logger.info('Stopping pinger...')
-        for i in tasks:
-            i.cancel()
-        await asyncio.gather(*tasks)
+    while True:
+        try:
+            tasks = []
+            hosts = await conf_init()
+            for host_name, host_ip in hosts.items():
+                tasks.append(asyncio.create_task(ping_compare(host_name, host_ip)))
+            while True:
+                await asyncio.sleep(1)
+                if await conf_init() != hosts:
+                    logger.info("Hosts changed, restarting")
+                    requests.get(
+                        f'https://api.telegram.org/bot1105929277:AAEHFVKbNdvPHNulpW-ywks8hozFpq3kNco/sendMessage?'
+                        f'chat_id=-1001206104553&parse_mode=html&message_thread_id=619&text=File hosts changed, restarting')
+                    for i in tasks:
+                        i.cancel()
+                    await asyncio.gather(*tasks)
+                    break
+        except KeyboardInterrupt:
+            logger.info('Stopping pinger...')
+            for i in tasks:
+                i.cancel()
+            await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
